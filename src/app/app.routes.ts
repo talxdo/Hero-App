2a096ab862ee8e778fc4536f74181fdae09b1fb3
talxdo <mccn.tala@gmail.com>
2024-10-26 13:55:31 -0400

    rutas y alias ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { CreateHeroComponent } from '@components/create-hero/create-hero.component';
+import { HeroDetailsComponent } from '@components/hero-details/hero-details.component';
+import { HeroListComponent } from '@components/hero-list/hero-list.component';
+import { HomeComponent } from '@components/home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'heroes',
+        component: HeroListComponent
+    },
+    {
+        path: 'hero',
+        component: HeroDetailsComponent
+    },
+    {
+        path: 'create-hero',
+        component: CreateHeroComponent
+    },
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: '**',
+        component: HeroListComponent
+    },
+
+];
