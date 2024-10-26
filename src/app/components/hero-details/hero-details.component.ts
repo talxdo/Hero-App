fea478355d727555f18e0cca67a2d0437d0e9b1b
talxdo <mccn.tala@gmail.com>
2024-10-26 19:33:14 -0400

    detalles heroe ok

--- src/app/components/hero-details/hero-details.component.ts
+++ src/app/components/hero-details/hero-details.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Hero } from '@interfaces/Hero';
+import { HeroService } from '@services/hero.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-hero-details',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './hero-details.component.html',
   styleUrl: './hero-details.component.css'
 })
 export class HeroDetailsComponent {
+  
+  private route = inject(ActivatedRoute);
+  private heroservice = inject(HeroService);
+
+  public hero = toSignal<Hero>(
+    this.route.params
+    .pipe(
+      switchMap(({id}) => this.heroservice.getHeroById(id))
+    )
+  )
 
 }
