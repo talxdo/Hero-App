80f20cb846cea0649af121878797cb8db9e66eb3
talxdo <mccn.tala@gmail.com>
2024-10-26 13:21:16 -0400

    Crear elementos del proyecto ok

---
+++ src/app/components/create-hero/create-hero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-hero',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-hero.component.html',
+  styleUrl: './create-hero.component.css'
+})
+export class CreateHeroComponent {
+
+}
---
+++ src/app/interfaces/Hero.ts
@@ -0,0 +1,8 @@
+export interface Hero { 
+    nombre : string,
+    editorial : string,
+    poderes : string[],
+    identidadSecreta : string,
+    debut : string,
+    imagen : string,
+}
