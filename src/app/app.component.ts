e2908f75bfae107457a60e4eadf20703b78dcfff
talxdo <mccn.tala@gmail.com>
2024-10-27 09:36:19 -0400

    flowbite ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from '@shared/footer/footer.component';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,10 @@ import { FooterComponent } from '@shared/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Hero-App';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
